b8edc856592db62a12e4beb090d90029005b3728
TARIK KAOUTAR <mr.tarik098@gmail.com>
2024-09-08 18:32:31 +0800

    add more algo tech rag

---
+++ rag_technique/simple_rag.py
@@ -0,0 +1,89 @@
+import os
+import sys
+from dotenv import load_dotenv
+
+
+sys.path.append(os.path.abspath(os.path.join(os.getcwd(), '..'))) # Add the parent directory to the path since we work with notebooks
+
+
+# Load environment variables from a .env file
+load_dotenv()
+
+# Set the OpenAI API key environment variable
+os.environ["OPENAI_API_KEY"] = os.getenv('OPENAI_API_KEY')
+
+def encode_pdf(path, chunk_size=1000, chunk_overlap=200):
+    """
+    Encodes a PDF book into a vector store using OpenAI embeddings.
+
+    Args:
+        path: The path to the PDF file.
+        chunk_size: The desired size of each text chunk.
+        chunk_overlap: The amount of overlap between consecutive chunks.
+
+    Returns:
+        A FAISS vector store containing the encoded book content.
+    """
+
+    # Load PDF documents
+    loader = PyPDFLoader(path)
+    documents = loader.load()
+
+    # Split documents into chunks
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size, chunk_overlap=chunk_overlap, length_function=len
+    )
+    texts = text_splitter.split_documents(documents)
+    cleaned_texts = replace_t_with_space(texts)
+
+    # Create embeddings and vector store
+    embeddings = OpenAIEmbeddings()
+    vectorstore = FAISS.from_documents(cleaned_texts, embeddings)
+
+    return vectorstore
+
+def retrieve_context_per_question(question, chunks_query_retriever):
+    """
+    Retrieves relevant context and unique URLs for a given question using the chunks query retriever.
+
+    Args:
+        question: The question for which to retrieve context and URLs.
+
+    Returns:
+        A tuple containing:
+        - A string with the concatenated content of relevant documents.
+        - A list of unique URLs from the metadata of the relevant documents.
+    """
+
+    # Retrieve relevant documents for the given question
+    docs = chunks_query_retriever.get_relevant_documents(question)
+
+    # Concatenate document content
+    # context = " ".join(doc.page_content for doc in docs)
+    context = [doc.page_content for doc in docs]
+
+    return context
+
+def show_context(context):
+    """
+    Display the contents of the provided context list.
+
+    Args:
+        context (list): A list of context items to be displayed.
+
+    Prints each context item in the list with a heading indicating its position.
+    """
+    for i, c in enumerate(context):
+        print(f"Context {i + 1}:")
+        print(c)
+        print("\n")
+
+
+chunks_vector_store = encode_pdf(path, chunk_size=1000, chunk_overlap=200)
+
+chunks_query_retriever = chunks_vector_store.as_retriever(search_kwargs={"k": 2})
+
+
+test_query = "What is the main cause of climate change?"
+context = retrieve_context_per_question(test_query, chunks_query_retriever)
+show_context(context)
